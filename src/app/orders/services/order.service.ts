e264e5a59b9d22c4a7e7d41ac5c6360fb4f7bd55
David Morales <dsmorales349@misena.edu.co>
2018-11-13 14:33:12 -0500

    se agrega pantalla de orden por id

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -9,10 +9,18 @@ export class OrderService {
 
   constructor(private _globalService: GlobalService) { }
 
+  order: any;
 
   getOrderByUserId(): Observable<any> {
     let id = JSON.parse(localStorage.getItem('user_data')).plac_user_id;
     return this._globalService.HttpMethod("GET", `tienda/store/orders/user/${id}/state`);
+
+  }
+
+  getOrderById(id): Observable<any> {
+    let plac_user_id = JSON.parse(localStorage.getItem('user_data')).plac_user_id;
+
+    return this._globalService.HttpMethod("GET", `orders/${id}`, { plac_user_id });
 
   }
 }
