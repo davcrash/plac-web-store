8a011d1b2a9324832e0685ba8de0e71a81b971c3
David Morales <dsmorales349@misena.edu.co>
2018-11-29 09:23:13 -0500

    se elaboro los baner y el mensaje en el perfil de la empresa

--- src/app/advertising/banner/banner.component.ts
+++ src/app/advertising/banner/banner.component.ts
@@ -8,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BannerComponent implements OnInit {
 
   imageUrls = [
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg'},
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg'},
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg'},
+    { url: 'https://res.cloudinary.com/plac-web/image/upload/v1543500997/page/banners/pieza-banner-web-avplac.jpg' }
   ];
 
   constructor() { }
