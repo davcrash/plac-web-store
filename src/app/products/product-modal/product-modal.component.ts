9c74cede341569f1902661cca8a442bd46d4ec9e
David Morales <dsmorales349@misena.edu.co>
2021-01-20 21:43:29 -0500

    fix products modal

--- src/app/products/product-modal/product-modal.component.ts
+++ src/app/products/product-modal/product-modal.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Location } from '@angular/common';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductModalService } from '../services/product-modal.service';
-import { ShopCartService } from '../../shop-cart/services/shop-cart.service';
-import swal from 'sweetalert';
-import { FormatService } from 'src/app/format.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Location } from "@angular/common";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductModalService } from "../services/product-modal.service";
+import { ShopCartService } from "../../shop-cart/services/shop-cart.service";
+import swal from "sweetalert";
+import { FormatService } from "src/app/format.service";
 
 @Component({
-  selector: 'app-product-modal',
-  templateUrl: './product-modal.component.html',
-  styleUrls: ['./product-modal.component.css']
+  selector: "app-product-modal",
+  templateUrl: "./product-modal.component.html",
+  styleUrls: ["./product-modal.component.css"],
 })
 export class ProductModalComponent implements OnInit {
-
-  @ViewChild('productModal') modal;//modal element
+  @ViewChild("productModal") modal; //modal element
 
   productId;
   product;
@@ -30,13 +29,13 @@ export class ProductModalComponent implements OnInit {
     place_id: "",
     plac_user_id: "",
     question_txt: "",
-  }
+  };
 
   textButtonQuestion = "Preguntar";
   questionGenerated;
   uid;
 
-  JSON;//para usar stringify y parse en el html
+  JSON; //para usar stringify y parse en el html
   constructor(
     private _modalService: NgbModal,
     private _route: ActivatedRoute,
@@ -46,86 +45,115 @@ export class ProductModalComponent implements OnInit {
     private _shopCartService: ShopCartService,
     private _formatService: FormatService
   ) {
-    this.JSON = JSON;//para usar stringify y parse en el html
+    this.JSON = JSON; //para usar stringify y parse en el html
   }
 
   ngOnInit() {
+    this.uid = localStorage.getItem("user_data")
+      ? JSON.parse(localStorage.getItem("user_data")).plac_user_id
+      : "";
 
-    this.uid = (localStorage.getItem("user_data")) ? JSON.parse(localStorage.getItem("user_data")).plac_user_id : '';
-
-    this._route.params.subscribe(routeParam => {
+    this._route.params.subscribe((routeParam) => {
       this.productId = routeParam.id;
       //this.productName = this._formatService.unformatString(routeParam.name);
-      this._productModalService.getProductById(this.productId)
-        .subscribe(res => {
-          this.product = res.data;
+      this._productModalService.getProductById(this.productId).subscribe(
+        (res) => {
+          this.product = res;
 
           if (!routeParam.name) {
             let queryParams = this._route.snapshot.queryParams;
-            let url = this._router.createUrlTree([this._formatService.formatString(this.product.product_name)], { relativeTo: this._route, queryParams })
+            let url = this._router
+              .createUrlTree(
+                [this._formatService.formatString(this.product.product_name)],
+                { relativeTo: this._route, queryParams }
+              )
               .toString();
             //this._router.navigate([url]);
             this._location.go(url);
           } else {
-            if (this.product.product_name.trim().toLowerCase() != this._formatService.unformatString(routeParam.name).toLowerCase()) {
+            if (
+              this.product.product_name.trim().toLowerCase() !=
+              this._formatService.unformatString(routeParam.name).toLowerCase()
+            ) {
               let queryParams = this._route.snapshot.queryParams;
-              let url = this._router.createUrlTree(
-                ['../' + this._formatService.formatString(this.product.product_name)], { relativeTo: this._route, queryParams }
-              )
+              let url = this._router
+                .createUrlTree(
+                  [
+                    "../" +
+                      this._formatService.formatString(
+                        this.product.product_name
+                      ),
+                  ],
+                  { relativeTo: this._route, queryParams }
+                )
                 .toString();
               //this._router.navigate([url]);
               this._location.go(url);
             }
           }
 
-
           this.product.questions.reverse();
           let maxLength = 170;
           if (this.product.product_description.length > maxLength) {
-
-            this.productShortDescription = this.product.product_description.substring(0, maxLength).trim().concat('...');
-            this.productViewDescription = this.productShortDescription
+            this.productShortDescription = this.product.product_description
+              .substring(0, maxLength)
+              .trim()
+              .concat("...");
+            this.productViewDescription = this.productShortDescription;
           } else {
             this.productViewDescription = this.product.product_description;
           }
 
           //Consultamos si esta en el carrito
-          this.productInCart = this._shopCartService.getProductInCart(this.product)
+          this.productInCart = this._shopCartService.getProductInCart(
+            this.product
+          );
           this.quantityToAddToCart = this.productInCart.quantity;
 
-          this.loader = false
-        }, error => {
-          console.log(error);
-        }, () => {//Cuando ya la solicitud se completo ocultamos el loader
           this.loader = false;
-        });
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          //Cuando ya la solicitud se completo ocultamos el loader
+          this.loader = false;
+        }
+      );
     });
   }
 
   ngAfterViewInit() {
-    setTimeout(() => {//el timeOut es para evitar un error 
+    setTimeout(() => {
+      //el timeOut es para evitar un error
 
-      //abrir modal 
-      this._modalService.open(this.modal, { size: 'lg' }).result.then(() => {
-        //regresar atras si se cierra
-        this.redirectCloseModal();
-      }, () => {
-        //regresar atras si se da click afuera
-        this.redirectCloseModal();
-
-      });
-
+      //abrir modal
+      this._modalService.open(this.modal, { size: "lg" }).result.then(
+        () => {
+          //regresar atras si se cierra
+          this.redirectCloseModal();
+        },
+        () => {
+          //regresar atras si se da click afuera
+          this.redirectCloseModal();
+        }
+      );
     }, 1);
-
   }
 
   redirectCloseModal() {
     var url;
     let queryParams = this._route.snapshot.queryParams;
     try {
-      url = this._router.createUrlTree(['../../../'], { relativeTo: this._route, queryParams }).toString().replace(/%20/g, ' ');
+      url = this._router
+        .createUrlTree(["../../../"], { relativeTo: this._route, queryParams })
+        .toString()
+        .replace(/%20/g, " ");
     } catch (error) {
-      url = this._router.createUrlTree(['../../'], { relativeTo: this._route, queryParams }).toString().replace(/%20/g, ' ');
+      url = this._router
+        .createUrlTree(["../../"], { relativeTo: this._route, queryParams })
+        .toString()
+        .replace(/%20/g, " ");
     }
     this._router.navigateByUrl(url);
     //(window.history.length > 2) ? this._location.back() : this._router.navigate(['../../../'], { relativeTo: this._route, replaceUrl: true });
@@ -134,95 +162,106 @@ export class ProductModalComponent implements OnInit {
   }
 
   viewMoreText() {
-    (this.product.product_description === this.productViewDescription) ? this.productViewDescription = this.productShortDescription : this.productViewDescription = this.product.product_description
+    this.product.product_description === this.productViewDescription
+      ? (this.productViewDescription = this.productShortDescription)
+      : (this.productViewDescription = this.product.product_description);
   }
 
   sendNewQuestion() {
-
     if (this.uid) {
       this.question.place_id = this.product.place_location.place_id;
       this.question.product_id = this.product.product_id;
       this.question.plac_user_id = this.uid;
 
       this.textButtonQuestion = "Enviando...";
-      this._productModalService.sendNewQuestion(this.question).subscribe(data => {
-        this.question.question_txt = '';
-        this.questionGenerated = data.data;
-        this.product.questions.unshift(this.questionGenerated);
+      this._productModalService.sendNewQuestion(this.question).subscribe(
+        (data) => {
+          this.question.question_txt = "";
+          this.questionGenerated = data.data;
+          this.product.questions.unshift(this.questionGenerated);
 
-        //this.product.questions.reverse();
-        this.textButtonQuestion = "Preguntar";
-      }, error => {
-        console.log(error);
-        this.textButtonQuestion = "Preguntar";
-      });
+          //this.product.questions.reverse();
+          this.textButtonQuestion = "Preguntar";
+        },
+        (error) => {
+          console.log(error);
+          this.textButtonQuestion = "Preguntar";
+        }
+      );
     } else {
       swal({
-        title: 'Inicia sesión',
-        text: 'Para realizar tus compras, primero debes iniciar sesión',
-        icon: 'info',
+        title: "Inicia sesión",
+        text: "Para realizar tus compras, primero debes iniciar sesión",
+        icon: "info",
         buttons: ["Más tarde", "Iniciar sesión"],
       }).then((value) => {
         if (value === true) {
-          this._router.navigate(['login']);
+          this._router.navigate(["login"]);
         }
       });
     }
-
-
-
   }
 
   addUnits() {
     if (this.quantityToAddToCart <= 50) {
-      this.quantityToAddToCart = (this.quantityToAddToCart + 1);
-      this.productInCart.totalProduct = this.product.product_price * this.quantityToAddToCart;
+      this.quantityToAddToCart = this.quantityToAddToCart + 1;
+      this.productInCart.totalProduct =
+        this.product.product_price * this.quantityToAddToCart;
     }
   }
 
   removeUnits() {
     if (this.quantityToAddToCart > 1) {
-      this.quantityToAddToCart = (this.quantityToAddToCart - 1);
-      this.productInCart.totalProduct = this.product.product_price * this.quantityToAddToCart;
+      this.quantityToAddToCart = this.quantityToAddToCart - 1;
+      this.productInCart.totalProduct =
+        this.product.product_price * this.quantityToAddToCart;
     }
   }
 
   addProductToCart() {
-    this._shopCartService.addProductToCart(this.product, this.quantityToAddToCart, true);
+    this._shopCartService.addProductToCart(
+      this.product,
+      this.quantityToAddToCart,
+      true
+    );
   }
 
-
   goToLogin() {
-
     setTimeout(() => {
-      this._router.navigate(['login']);
+      this._router.navigate(["login"]);
     }, 16);
-
   }
 
   shareProduct(destination) {
     console.log(this.product.product_dynamic_link);
     switch (destination) {
-      case 'facebook':
-        window.open(`http://www.facebook.com/sharer.php?u=${this.product.product_dynamic_link}`,
-          'facebookwindow',
-          'toolbar=0, status=0, width=650, height=650');
+      case "facebook":
+        window.open(
+          `http://www.facebook.com/sharer.php?u=${this.product.product_dynamic_link}`,
+          "facebookwindow",
+          "toolbar=0, status=0, width=650, height=650"
+        );
         break;
-      case 'pinterest':
-        window.open(`http://www.pinterest.com/pin/create/bookmarklet/?url=${this.product.product_dynamic_link}&media=${JSON.parse(this.product.product_images)[0].url}&is_video=false&description=${this.product.product_description}`,
-          'pinterestwindow',
-          'toolbar=0, status=0, width=650, height=650');
+      case "pinterest":
+        window.open(
+          `http://www.pinterest.com/pin/create/bookmarklet/?url=${this.product.product_dynamic_link}&media=${this.product.product_images[0].url}&is_video=false&description=${this.product.product_description}`,
+          "pinterestwindow",
+          "toolbar=0, status=0, width=650, height=650"
+        );
         break;
-      case 'twitter':
-
-        window.open(`https://twitter.com/intent/tweet?url=${this.product.product_dynamic_link}&text=${this.product.product_description.replace("%", "%25")}&via=placapp&`,
-          'twitterwindow',
-          'toolbar=0, status=0, width=650, height=650');
+      case "twitter":
+        window.open(
+          `https://twitter.com/intent/tweet?url=${
+            this.product.product_dynamic_link
+          }&text=${this.product.product_description.replace(
+            "%",
+            "%25"
+          )}&via=placapp&`,
+          "twitterwindow",
+          "toolbar=0, status=0, width=650, height=650"
+        );
 
         break;
-
     }
-
   }
-
 }
--- src/app/products/services/product-modal.service.ts
+++ src/app/products/services/product-modal.service.ts
@@ -1,20 +1,63 @@
-import { Injectable } from '@angular/core';
-import { GlobalService } from '../../global.service';
-import { Observable } from 'rxjs';
-
+import { Injectable } from "@angular/core";
+import { GlobalService } from "../../global.service";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductModalService {
-
-  constructor(private _globalService: GlobalService) { }
+  constructor(private _globalService: GlobalService) {}
 
   getProductById(id): Observable<any> {
-    return this._globalService.HttpMethod("GET", `products/${id}`);
+    return new Observable((subscriber) => {
+      const random_boolean = Math.random() < 0.5;
+      if (random_boolean) {
+        subscriber.next({
+          product_name: "Cama tibuton comelon para cachorro talla xs",
+          product_price: 40250,
+          product_images: [
+            {
+              url:
+                "/assets/img/product-images/producttiburon2p290c95b6badc062al250.jpg",
+            },
+          ],
+          product_description:
+            "Cama super confortable para tu cachorro lorem ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó",
+          questions: [
+            {
+              question_txt: "Que tallas tienes disponibles?",
+              answer_txt: "Hola, Tenemos en talla s talla m y talla este",
+            },
+          ],
+        });
+      } else {
+        subscriber.next({
+          product_name: "Peluche conejo BadBunny Brrr para conejos malos",
+          product_price: 19900,
+          product_images: [
+            {
+              url: "/assets/img/product-images/product1IMGixJ33z7owj.jpg",
+            },
+          ],
+          product_description:
+            "Peluche super confortable para tu cachorro lorem ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó",
+          questions: [
+            {
+              question_txt: "Que tallas tienes disponibles?",
+              answer_txt: "Hola, Tenemos en talla s talla m y talla este",
+            },
+            {
+              question_txt: "Donde estan ubicados?",
+              answer_txt:
+                "ipsum es simplemente el texto de relleno de las ipsum es simplemente el texto de relleno de las",
+            },
+          ],
+        });
+      }
+    });
   }
 
   sendNewQuestion(question): Observable<any> {
-    return this._globalService.HttpMethod("POST", "products/questions", question);
+    return;
   }
 }
