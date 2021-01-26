782ba44958b1ff6ac35ff5c9ab4de3646cfc4d7c
David Morales <dsmorales349@misena.edu.co>
2021-01-26 12:10:59 -0500

    delete orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,85 +1,93 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { PageNotFoundComponent } from './main/page-not-found/page-not-found.component';
-import { LoginComponent } from './users/login/login.component';
-import { RegisterComponent } from './users/register/register.component';
-import { HomePageComponent } from './main/home-page/home-page.component';
-import { ProductModalComponent } from './products/product-modal/product-modal.component';
-import { PlaceProfileComponent } from './places/place-profile/place-profile.component';
-import { CategoryWithFiltersComponent } from './categories/category-with-filters/category-with-filters.component';
-import { SearchComponent } from './main/search/search.component';
-import { RegisterGuard } from './users/register/register.guard';
-import { LoginGuard } from './users/login/login.guard';
-import { PurchaseViewComponent } from './shop-cart/purchase-view/purchase-view.component';
-import { DownloadAppComponent } from './main/landing/download-app/download-app.component';
-import { SellInComponent } from './main/landing/sell-in/sell-in.component';
-import { WhatIsComponent } from './main/landing/what-is/what-is.component';
-import { TeamComponent } from './main/landing/team/team.component';
-import { PressComponent } from './main/landing/press/press.component';
-import { JobComponent } from './main/landing/job/job.component';
-import { OrdersListComponent } from './orders/orders-list/orders-list.component';
-import { ChangesReturnsComponent } from './main/landing/changes-returns/changes-returns.component';
-import { OrderViewComponent } from './orders/order-view/order-view.component';
-
-
+import { PageNotFoundComponent } from "./main/page-not-found/page-not-found.component";
+import { LoginComponent } from "./users/login/login.component";
+import { RegisterComponent } from "./users/register/register.component";
+import { HomePageComponent } from "./main/home-page/home-page.component";
+import { ProductModalComponent } from "./products/product-modal/product-modal.component";
+import { PlaceProfileComponent } from "./places/place-profile/place-profile.component";
+import { CategoryWithFiltersComponent } from "./categories/category-with-filters/category-with-filters.component";
+import { SearchComponent } from "./main/search/search.component";
+import { RegisterGuard } from "./users/register/register.guard";
+import { LoginGuard } from "./users/login/login.guard";
+import { PurchaseViewComponent } from "./shop-cart/purchase-view/purchase-view.component";
+import { DownloadAppComponent } from "./main/landing/download-app/download-app.component";
+import { SellInComponent } from "./main/landing/sell-in/sell-in.component";
+import { WhatIsComponent } from "./main/landing/what-is/what-is.component";
+import { TeamComponent } from "./main/landing/team/team.component";
+import { PressComponent } from "./main/landing/press/press.component";
+import { JobComponent } from "./main/landing/job/job.component";
+import { ChangesReturnsComponent } from "./main/landing/changes-returns/changes-returns.component";
 
 const routes: Routes = [
   //login
-  { path: 'login', component: LoginComponent },
+  { path: "login", component: LoginComponent },
 
   //registrar usuario
-  { path: 'register', component: RegisterComponent, canActivate: [RegisterGuard] },
+  {
+    path: "register",
+    component: RegisterComponent,
+    canActivate: [RegisterGuard],
+  },
   //home
   {
-    path: '', component: HomePageComponent,
+    path: "",
+    component: HomePageComponent,
     children: [
-      { path: 'producto/:id', component: ProductModalComponent },
-      { path: 'producto/:id/:name', component: ProductModalComponent }
-    ]
+      { path: "producto/:id", component: ProductModalComponent },
+      { path: "producto/:id/:name", component: ProductModalComponent },
+    ],
   },
   //perfil empresa
   {
-    path: 'tienda/:name', component: PlaceProfileComponent,
+    path: "tienda/:name",
+    component: PlaceProfileComponent,
     children: [
-      { path: 'producto/:id/:name', component: ProductModalComponent },
-      { path: 'producto/:id', component: ProductModalComponent }
-    ]
+      { path: "producto/:id/:name", component: ProductModalComponent },
+      { path: "producto/:id", component: ProductModalComponent },
+    ],
   },
   //categoria
   {
-    path: 'categoria/:category_name', component: CategoryWithFiltersComponent,
+    path: "categoria/:category_name",
+    component: CategoryWithFiltersComponent,
     children: [
-      { path: 'producto/:id/:name', component: ProductModalComponent },
-      { path: 'producto/:id', component: ProductModalComponent }
-    ]
+      { path: "producto/:id/:name", component: ProductModalComponent },
+      { path: "producto/:id", component: ProductModalComponent },
+    ],
   },
   //busqueda
   {
-    path: 'search', component: SearchComponent,
+    path: "search",
+    component: SearchComponent,
     children: [
-      { path: 'producto/:id/:name', component: ProductModalComponent },
-      { path: 'producto/:id', component: ProductModalComponent }
-    ]
+      { path: "producto/:id/:name", component: ProductModalComponent },
+      { path: "producto/:id", component: ProductModalComponent },
+    ],
   },
 
   //rutas para dynamic links
-  { path: 'notificaciones/:noti', component: DownloadAppComponent },
-  { path: 'perfil/:perf', component: DownloadAppComponent },
+  { path: "notificaciones/:noti", component: DownloadAppComponent },
+  { path: "perfil/:perf", component: DownloadAppComponent },
 
-  { path: 'compra/:indexPlace', component: PurchaseViewComponent, canActivate: [LoginGuard] },
-  { path: 'descargar', component: DownloadAppComponent },
-  { path: 'vende-en-plac', component: SellInComponent },
-  { path: 'vende-en-plac/:destination', component: SellInComponent },
-  { path: 'que-es-plac', component: WhatIsComponent },
-  { path: 'equipo', component: TeamComponent },
-  { path: 'prensa', component: PressComponent },
-  { path: 'empleo', component: JobComponent },
-  { path: 'ordenes', component: OrdersListComponent, canActivate: [LoginGuard] },
-  { path: 'orden/:id/:pago', component: OrderViewComponent, canActivate: [LoginGuard] },
-  { path: 'orden/:id', component: OrderViewComponent, canActivate: [LoginGuard] },
-  { path: 'descargar', component: DownloadAppComponent },
-  { path: 'politica-cambios-y-devoluciones', component: ChangesReturnsComponent },
+  {
+    path: "compra/:indexPlace",
+    component: PurchaseViewComponent,
+    canActivate: [LoginGuard],
+  },
+  { path: "descargar", component: DownloadAppComponent },
+  { path: "vende-en-plac", component: SellInComponent },
+  { path: "vende-en-plac/:destination", component: SellInComponent },
+  { path: "que-es-plac", component: WhatIsComponent },
+  { path: "equipo", component: TeamComponent },
+  { path: "prensa", component: PressComponent },
+  { path: "empleo", component: JobComponent },
+  { path: "descargar", component: DownloadAppComponent },
+  {
+    path: "politica-cambios-y-devoluciones",
+    component: ChangesReturnsComponent,
+  },
 
   /*
   EJEMPLOS
@@ -89,13 +97,11 @@ const routes: Routes = [
   { path: 'users/:id', component: UserFormComponent },
   */
 
-  { path: '**', component: PageNotFoundComponent },
-
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
