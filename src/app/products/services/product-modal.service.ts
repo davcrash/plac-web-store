fbde769e177f2fde0fb58726a6052a617b04cb49
David Morales <dsmorales349@misena.edu.co>
2021-01-27 22:19:41 -0500

    fix product modal service

--- src/app/products/services/product-modal.service.ts
+++ src/app/products/services/product-modal.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { GlobalService } from "../../global.service";
 import { Observable } from "rxjs";
+import { places, products } from "src/app/data";
 
 @Injectable({
   providedIn: "root",
@@ -9,77 +10,31 @@ export class ProductModalService {
   constructor(private _globalService: GlobalService) {}
 
   getProductById(id): Observable<any> {
-    return new Observable((subscriber) => {
-      const random_boolean = Math.random() < 0.5;
-      if (random_boolean) {
-        subscriber.next({
-          product_id: 1,
-          product_name: "Cama tibuton comelon para cachorro talla xs",
-          product_price: 40250,
-          product_images: [
-            {
-              url:
-                "/assets/img/product-images/producttiburon2p290c95b6badc062al250.jpg",
-            },
-          ],
-          product_link_image:
-            "/assets/img/product-images/producttiburon2p290c95b6badc062al250.jpg",
-          place_location: {
-            place_location_name: "MasVital",
-            place_description: "Tienda de alimento organico para mascotas",
-            place: {
-              path_image_logo:
-                "/assets/img/place-imgs/productMVPAMIXS603256900lc62664cc6bapd.jpg",
-            },
-            place_id: 1,
+    return new Observable((s) => {
+      const product = products.find((product) => product.product_id == id);
+      const place = places[1];
+      s.next({
+        ...product.product,
+        product_id: product.product_id,
+        product_link_image: product.product.product_images[0].url,
+        place_id: place.place_id,
+        place_location: {
+          place_location_name: place.place_location.place_location_name,
+          place_description: place.place_location.place_description,
+          place: {
+            path_image_logo: place.path_image_logo,
           },
-          place_id: 1,
-          product_description:
-            "Cama super confortable para tu cachorro lorem ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó",
-          questions: [
-            {
-              question_txt: "Que tallas tienes disponibles?",
-              answer_txt: "Hola, Tenemos en talla s talla m y talla este",
-            },
-          ],
-        });
-      } else {
-        subscriber.next({
-          product_id: 2,
-          product_name: "Peluche conejo BadBunny Brrr para conejos malos",
-          product_price: 19900,
-          product_images: [
-            {
-              url: "/assets/img/product-images/product1IMGixJ33z7owj.jpg",
-            },
-          ],
-          product_link_image:
-            "/assets/img/product-images/product1IMGixJ33z7owj.jpg",
-          place_location: {
-            place_location_name: "Pawceros",
-            place_description: "Tienda de gatos en medallo pa pa!",
-            place: {
-              path_image_logo:
-                "/assets/img/place-imgs/productMichi8054521215eeabpl0abd20c.png",
-            },
-            place_id: 1,
+          place_id: place.place_id,
+        },
+        product_description:
+          "Super confortable para tu cachorro lorem ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó",
+        questions: [
+          {
+            question_txt: "Que tallas tienes disponibles?",
+            answer_txt: "Hola, Tenemos en talla s talla m y talla este",
           },
-          place_id: 1,
-          product_description:
-            "Peluche super confortable para tu cachorro lorem ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó",
-          questions: [
-            {
-              question_txt: "Que tallas tienes disponibles?",
-              answer_txt: "Hola, Tenemos en talla s talla m y talla este",
-            },
-            {
-              question_txt: "Donde estan ubicados?",
-              answer_txt:
-                "ipsum es simplemente el texto de relleno de las ipsum es simplemente el texto de relleno de las",
-            },
-          ],
-        });
-      }
+        ],
+      });
     });
   }
 
